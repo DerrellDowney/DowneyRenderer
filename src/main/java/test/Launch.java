118dff11e2876f77fd4f5a37f310930558ef7cb8
DerrellDowney <derrell.downey@gmail.com>
2023-01-13 04:34:50 -0500

    Window Setup

--- src/main/java/test/Launch.java
+++ src/main/java/test/Launch.java
@@ -1,10 +1,19 @@
 package test;
 
+import core.WindowManager;
 import org.lwjgl.Version;
 
 public class Launch {
     public static void main(String [] args){
         System.out.println(Version.getVersion());
+        WindowManager window = new WindowManager("DOWNEY ENGINE", 0, 0, false);
+        window.init();
+
+        while(!window.windowShouldClose()){
+            window.update();
+        }
+
+        window.cleanup();
     }
 
 }
